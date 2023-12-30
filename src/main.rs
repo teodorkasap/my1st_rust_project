c015557bc49c1fd5baacac6ebb0ca2ca0ef34f57
Erol Erten <erol.erten@gmail.com>
2023-12-30 22:42:31 +0300

    Changes to be committed: 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 use regex::Regex;
+use reqwest::Error;
 
-fn main() {
-    // println!("Hello, world!");
+
+async fn get_request() -> Result<(), Error> {
+    let response = reqwest::get("https://www.fruityvice.com/api/fruit/apple").await?;
+    println!("Status: {}", response.status());
+
+    let body = response.text().await?;
+    println!("Body:\n{}", body);
+
+    Ok(())
+}
+
+
+#[tokio::main]
+
+async fn main() -> Result<(), Error> {
+    
+    println!("Hello, world!");
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2014-01-01"));
+
+    get_request().await?;
+    Ok(())
+ 
+
+    
 }
